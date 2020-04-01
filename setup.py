ebaf13ad15fbc30a1b1ca17ed062f3b285b8e18a
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-01 09:08:04 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Package meta-data
+NAME = "transcov"
+DESCRIPTION = ""
+
+setup(
+    name = "transcov",
+    description = "A software for mapping coverage around transcription start sites",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    #test_suite="test",
+    install_requires=[],
+    entry_points={
+         'console_scripts': ['transcov = transcov.cli:start'],
+    },
+    include_package_data=True,
+)
+
