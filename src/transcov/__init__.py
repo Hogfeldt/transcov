4de879942f41435d0388c843a9992f147c8725a1
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-03 13:31:11 +0200

    Added autodoc to the api and imported core functions in __init__

---
+++ src/transcov/__init__.py
@@ -0,0 +1,7 @@
+from .generator import generate_coverage_matrix
+from .preprocessor import preprocess
+from .collapser import collapse
+
+__version__ = "1.0.0"
+
+__all__ = ("generate_coverage_matrix", "preprocess", "collapse")
