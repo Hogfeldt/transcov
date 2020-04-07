825da5e0fc95d8513ed2c72a8e3f85e505b41bfe
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-07 11:44:56 +0200

    slacked dependencies

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     package_dir={"": "src"},
     # test_suite="test",
     install_requires=[
-        "pysam>=0.15.4",
-        "numpy>=1.18.1",
-        "attrs>=19.3.0",
-        "click>=7.1.1",
+        "pysam",        # 0.15.4
+        "numpy",        # 1.18.1
+        "attrs",        # 19.3.0
+        "click>=6.0",
     ],
     entry_points={"console_scripts": ["transcov = transcov.cli:cli"],},
     include_package_data=True,
