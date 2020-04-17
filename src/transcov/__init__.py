769e217236056d8e920cb5ec1815e164f74ca725
Per Høgfeldt <hogfeldt38@gmail.com>
2020-04-17 13:31:59 +0200

    Update __init__.py

--- src/transcov/__init__.py
+++ src/transcov/__init__.py
@@ -1,4 +1,4 @@
-from .generator import generate_coverage_matrix
+from .generator import generate_coverage_matrix, generate_read_ends_matrix, generate_length_matrix
 from .preprocessor import preprocess
 from .collapser import collapse
 
