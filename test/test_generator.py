8622d97e4d99a2122e89052dea84bac06d7dd082
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-17 11:06:53 +0200

    Added test for generator to the whole module

--- test/test_generator.py
+++ test/test_generator.py
@@ -1,22 +1,11 @@
 import unittest
 import numpy as np
+import tempfile
+import filecmp
+from os.path import join, dirname
+
 from transcov import generator
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
 
 class Test_calc_rel_start_and_end_Function(unittest.TestCase):
 
@@ -71,6 +60,92 @@ class Test_add_read_ends_Function(unittest.TestCase):
         res[end + self.k] = 1
         self.assertTrue((self.A[i]==res).all())
 
+    def test_left_out_of_bounds_fragment(self):
+        start = -300
+        end = 30
+        i = 0
+        generator.add_read_ends(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[end + self.k] = 1
+        self.assertTrue((self.A[i]==res).all())
+
+    def test_addtion_side_effect_fragment(self):
+        start = -300
+        end = 30
+        i = 1
+        generator.add_read_ends(self.A, start, end, i, self.k)
+        generator.add_read_ends(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[end + self.k] = 2
+        self.assertTrue((self.A[i]==res).all())
+        
+    def test_edges_fragment(self):
+        start = -50
+        end = 50
+        i = 2
+        generator.add_read_ends(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[start + self.k] = 1
+        res[end + self.k] = 1
+        self.assertTrue((self.A[i]==res).all())
+
+class Test_add_fragment_Function(unittest.TestCase):
+
+    def setUp(self):
+        self.A = np.zeros((3,101), dtype=np.uint16)
+        self.k = 50
+
+    def test_add_center_fragment(self):
+        start = -30
+        end = 30
+        i = 1
+        generator.add_fragment(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[start+self.k:end+self.k] = 1
+        self.assertTrue((self.A[i]==res).all())
+
+    def test_left_out_of_bounds_fragment(self):
+        start = -300
+        end = 30
+        i = 0
+        generator.add_fragment(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[start+self.k:end+self.k] = 1
+        self.assertTrue((self.A[i]==res).all())
+
+    def test_addtion_side_effect_fragment(self):
+        start = -300
+        end = 30
+        i = 1
+        generator.add_fragment(self.A, start, end, i, self.k)
+        generator.add_fragment(self.A, start, end, i, self.k)
+        res = np.zeros(101, dtype=np.uint16)
+        res[start+self.k:end+self.k] = 2
+        self.assertTrue((self.A[i]==res).all())
+        
+    def test_edges_fragment(self):
+        start = -50
+        end = 51
+        i = 2
+        generator.add_fragment(self.A, start, end, i, self.k)
+        res = np.ones(101, dtype=np.uint16)
+        self.assertTrue((self.A[i]==res).all())
+
+class Test_module_with_file_io(unittest.TestCase):
+    
+    def setUp(self):
+        self.output_matrix = tempfile.NamedTemporaryFile() 
+
+    def tearDown(self):
+        self.output_matrix.close()
+
+    def test_matrix_generation(self):
+        bam_file = join(dirname(__file__), 'data/test.bam')
+        bed_file = join(dirname(__file__), 'data/gencode.v19.annotation.tss.bed')
+        res_matrix = join(dirname(__file__), 'data/coverage_matrix_read_ends.npy')
+        generator.generate_coverage_matrix(bam_file, bed_file, self.output_matrix.name)
+        res = filecmp.cmp(res_matrix, self.output_matrix.name + '.npy', shallow=False)
+        self.assertTrue(res)
 
 if __name__=='__main__':
     unittest.main()
