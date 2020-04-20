a5716f23c4c40874823d4ed2e9e1a7bc252d9a7a
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-20 15:18:51 +0200

    Added pick_subset manipulation

--- src/transcov/__init__.py
+++ src/transcov/__init__.py
@@ -1,7 +1,12 @@
 from .generator import generate_coverage_matrix, generate_read_ends_matrix, generate_length_matrix
 from .preprocessor import preprocess
-from .collapser import collapse
+from .manipulations import collapse, pick_subset
 
 __version__ = "1.0.4"
 
-__all__ = ("generate_coverage_matrix", "generate_read_ends_matrix", "generate_length_matrix", "preprocess", "collapse")
+__all__ =  ("generate_coverage_matrix", 
+            "generate_read_ends_matrix", 
+            "generate_length_matrix", 
+            "preprocess", 
+            "collapse",
+            "pick_subset",)
--- src/transcov/manipulations.py
+++ src/transcov/manipulations.py
@@ -1,6 +1,60 @@
 import argparse
 import numpy as np
+from functools import partial
+from itertools import tee, count
 
+from .utils import tsv_reader, write_matrix_and_index_file
+
+def create_index_map(index_file):
+    index_map = dict()
+    with open(index_file) as fp:
+        for line in tsv_reader(fp):
+            if line[0].startswith('#'):
+                continue
+            index_map[line[1]] = int(line[0])
+    return index_map
+
+def pick_subset_by_row_index(X, index_pairs, n):
+    _, m = X.shape
+    A = np.zeros((n, m), dtype=X.dtype)
+    for i, j in index_pairs:
+        A[i] = X[j]
+    return A
+
+pairing = lambda index_map, row_id: (row_id, index_map[row_id])
+
+def unzip_pair_iter(it):
+    it1, it2  = tee(it)
+    first_it  = (a for a, b in it1)
+    second_it = (b for a, b in it2)
+    return (first_it, second_it)
+
+def get_id_index_pair_iters(index_map, ids):
+    pair_iter = map(partial(pairing, index_map), ids)
+    ids_iter, index_iter = unzip_pair_iter(pair_iter)
+    new_index_id_pairs = zip(count(), ids_iter)
+    new_old_index_pairs = zip(count(), index_iter)
+    return (new_index_id_pairs, new_old_index_pairs)
+
+def pick_subset(matrix_file, index_file, output_file, ids):
+    """ Given a matrix and a list of row identifiers, create a 
+        new matrix which is a row-wise subset of the given matrix.
+
+        :param matrix_file: File path to the input matrix
+        :type matrix_file: str
+        :param index_file: File path to the index file which coresponds to the input matrix
+        :type index_file: str
+        :param output_file: File path to the output matrix
+        :type output_file: str
+        :param ids: List of row identifiers for the input matrix
+        :type start: List[str]
+        :returns:  None
+    """
+    matrix = np.load(matrix_file)
+    index_map = create_index_map(index_file)
+    new_index_id_pairs, new_old_index_pairs = get_id_index_pair_iters(index_map, ids)
+    sub_matrix = pick_subset_by_row_index(matrix, new_old_index_pairs, len(ids))
+    write_matrix_and_index_file(output_file, sub_matrix, new_index_id_pairs)
 
 def collapse(matrices, output_file, start=0, end=None, uint32=False):
     """ This function will given a list of matrices, load in the matrices one by
